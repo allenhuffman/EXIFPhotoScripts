a11bf05ef081f9d4087f2a1f91c95d6c19c97b68
Allen Huffman <alsplace@pobox.com>
2023-03-13 12:25:00 -0500

    Command line help, slight speedup

--- find_exif_dates.sh
+++ find_exif_dates.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+# Function to print the help screen
+print_help() {
+  echo "Usage: $0 [directory]"
+  echo "Recursively searches for JPEG files in [directory] (default: current directory) and reports the unique dates on which photos were taken."
+  echo "Options:"
+  echo "  -h, --help    Print this help message"
+  exit 0
+}
+
+# Check if the user has requested the help screen
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  print_help
+fi
+
 # Get the directory to scan from the command line, or use the current directory if none is specified
 dir=${1:-.}
 
-# Initialize a counter for the number of JPEG files found and processed
+# Initialize a counter for the number of JPEG files found
 jpg_count=0
 
-# Initialize a counter for the number of files processed
-file_count=0
+# Initialize an array to keep track of unique dates
+unique_dates=()
 
-# Initialize a counter for the number of unique dates found
-unique_date_count=0
-
-# Create an empty dates.txt file in the directory being scanned
-touch "${dir}/dates.txt"
-
-# Recursively search for JPEG files in the specified directory and its subdirectories
+# Recursively search for JPEG files in the current directory and its subdirectories
 find "$dir" -type f -name "*.jpg" | while read filename; do
-  # Skip files that start with a dot
-  if [[ $filename == .* ]]; then
+  # Skip files that start with "."
+  if [[ "$(basename "$filename")" =~ ^\. ]]; then
     continue
   fi
-  
-  # Increment the file counter
-  file_count=$((file_count+1))
+
+  # Increment the JPEG file counter
+  jpg_count=$((jpg_count+1))
+
+  # Print status every 100 files processed
+  if (( $jpg_count % 100 == 0 )); then
+    echo "Processed $jpg_count files, unique dates found so far: ${#unique_dates[@]}"
+  fi
 
   # Extract the EXIF date from the file using ExifTool
   exif_date=$(exiftool "$filename" | grep "Date/Time Original" | awk '{print $4}')
@@ -31,27 +44,21 @@ find "$dir" -type f -name "*.jpg" | while read filename; do
   # Skip files with -1 as the date
   if [[ $exif_date == "-1" ]]; then
     continue
-  fi
-  
-  # Increment the JPEG file counter
-  jpg_count=$((jpg_count+1))
-
   # Check if the date has been seen before
-  if ! grep -q "$exif_date" "${dir}/dates.txt"; then
-    # If the date is unique, add it to the list of dates and increment the unique date counter
+  elif [[ " ${unique_dates[@]} " =~ " ${exif_date} " ]]; then
+    continue
+  else
+    # If the date is unique, add it to the list of dates and print it to the console
+    unique_dates+=("$exif_date")
     echo "$exif_date" >> "${dir}/dates.txt"
-    unique_date_count=$((unique_date_count+1))
-  fi
-  
-  # Print status every 100 files processed
-  if (( file_count % 100 == 0 )); then
-    echo "Processed $file_count files, found $jpg_count JPEG files, $unique_date_count unique dates"
   fi
 done
 
 # Report the number of files, JPEG files, and unique dates found
-echo "Processed $file_count files, found $jpg_count JPEG files, $unique_date_count unique dates"
+echo "Processed $jpg_count files, found $jpg_count JPEG files, ${#unique_dates[@]} unique dates"
 
 # Report the unique dates on which photos were taken
 echo "Unique dates on which photos were taken:"
 cat "${dir}/dates.txt"
+
+# End of file.
